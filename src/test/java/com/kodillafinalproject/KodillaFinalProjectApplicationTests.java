f5e759957a81544c2fb4a5075258e8117f74fff3
Miłosz Sierakowski <roland_m1@wp.pl>
2023-03-30 19:26:27 +0200

    0.0 first commit

---
+++ src/test/java/com/kodillafinalproject/KodillaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillafinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
